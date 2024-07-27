fc11e1ed1b055db0a454fc0170668625cf98bf21
FrancoisNWU <40723380@mynwu.ac.za>
2024-07-27 13:52:50 +0200

    Edit MemberForm

--- CMPG223_Group10_DVD_Rental/Member.Designer.cs
+++ CMPG223_Group10_DVD_Rental/Member.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(419, 130);
+            this.button1.Location = new System.Drawing.Point(354, 198);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
