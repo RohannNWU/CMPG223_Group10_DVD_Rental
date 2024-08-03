8cf915563ececfb87b29820fd5230ebd43fbbb52
RohannNWU <25130757@mynwu.ac.za>
2024-08-03 11:27:36 +0200

    Login Form updated
    Modified login form GUI

--- CMPG223_Group10_DVD_Rental/Login.Designer.cs
+++ CMPG223_Group10_DVD_Rental/Login.Designer.cs
@@ -33,12 +33,13 @@
             this.usernameTextBox = new System.Windows.Forms.MaskedTextBox();
             this.passwordTextBox = new System.Windows.Forms.MaskedTextBox();
             this.passwordLabel = new System.Windows.Forms.Label();
+            this.loginHeaderLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(176, 158);
-            this.btnLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLogin.Location = new System.Drawing.Point(190, 237);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(100, 28);
             this.btnLogin.TabIndex = 0;
@@ -46,16 +47,10 @@
             this.btnLogin.UseVisualStyleBackColor = true;
             this.btnLogin.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Login
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnLogin);
             // usernameLabel
             // 
             this.usernameLabel.AutoSize = true;
-            this.usernameLabel.Location = new System.Drawing.Point(65, 43);
+            this.usernameLabel.Location = new System.Drawing.Point(79, 116);
             this.usernameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.usernameLabel.Name = "usernameLabel";
             this.usernameLabel.Size = new System.Drawing.Size(136, 16);
@@ -64,14 +59,14 @@
             // 
             // usernameTextBox
             // 
-            this.usernameTextBox.Location = new System.Drawing.Point(226, 40);
+            this.usernameTextBox.Location = new System.Drawing.Point(240, 113);
             this.usernameTextBox.Name = "usernameTextBox";
             this.usernameTextBox.Size = new System.Drawing.Size(198, 22);
             this.usernameTextBox.TabIndex = 2;
             // 
             // passwordTextBox
             // 
-            this.passwordTextBox.Location = new System.Drawing.Point(226, 97);
+            this.passwordTextBox.Location = new System.Drawing.Point(240, 170);
             this.passwordTextBox.Name = "passwordTextBox";
             this.passwordTextBox.Size = new System.Drawing.Size(198, 22);
             this.passwordTextBox.TabIndex = 4;
@@ -79,25 +74,40 @@
             // passwordLabel
             // 
             this.passwordLabel.AutoSize = true;
-            this.passwordLabel.Location = new System.Drawing.Point(65, 100);
+            this.passwordLabel.Location = new System.Drawing.Point(79, 173);
             this.passwordLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.passwordLabel.Name = "passwordLabel";
             this.passwordLabel.Size = new System.Drawing.Size(133, 16);
             this.passwordLabel.TabIndex = 3;
             this.passwordLabel.Text = "Enter your Password:";
             // 
+            // loginHeaderLabel
+            // 
+            this.loginHeaderLabel.AutoSize = true;
+            this.loginHeaderLabel.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginHeaderLabel.Location = new System.Drawing.Point(96, 25);
+            this.loginHeaderLabel.Name = "loginHeaderLabel";
+            this.loginHeaderLabel.Size = new System.Drawing.Size(311, 40);
+            this.loginHeaderLabel.TabIndex = 5;
+            this.loginHeaderLabel.Text = "G10 DVD RENTALS";
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(498, 222);
+            this.ClientSize = new System.Drawing.Size(498, 319);
+            this.Controls.Add(this.loginHeaderLabel);
             this.Controls.Add(this.passwordTextBox);
             this.Controls.Add(this.passwordLabel);
             this.Controls.Add(this.usernameTextBox);
             this.Controls.Add(this.usernameLabel);
             this.Controls.Add(this.btnLogin);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -111,5 +121,6 @@
         private System.Windows.Forms.MaskedTextBox usernameTextBox;
         private System.Windows.Forms.MaskedTextBox passwordTextBox;
         private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Label loginHeaderLabel;
     }
 }
