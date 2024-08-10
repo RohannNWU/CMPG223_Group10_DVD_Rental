99462a3e4a5bcbb68690c2d9a235ad2cff5ee943
Jacques van Heerden <35317906@mynwu.ac.za>
2024-08-10 13:47:16 -0400

    Review
    Secure code

---
+++ CMPG223_Group10_DVD_Rental/Login.cs
@@ -0,0 +1,90 @@
+﻿/*
+ *
+ * CMPG223 Project - G10 DVD Rentals
+ * Date Created: 03/08/2024
+ * Rohann Venter, 25130757
+ * Jacques van Heerden, 35317906 
+ * Francois Verster, 40723380
+ * Stefan Venter, 39066894
+ * Christo Prinsloo, 21052239
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMPG223_Group10_DVD_Rental
+{
+    public partial class Login : Form
+    {
+        private string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\DVDRentalShop.mdf;Integrated Security=True";
+        private SqlConnection conn;
+        private SqlCommand command;
+        private SqlDataReader reader;
+        private string sqlQuery;
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        // login button validates the user and gets their role in the organization
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(connectionString);
+
+            try
+            {
+                //Jacques was hier om te review - secure code - thumbs up
+                conn.Open();
+                sqlQuery = @"SELECT * From dbo.Employee WHERE Username = @username AND Password = @password"; // preventing SQL injection by using parameters
+                command = new SqlCommand(sqlQuery, conn);
+                if (!String.IsNullOrEmpty(usernameTextBox.Text) || !String.IsNullOrEmpty(passwordTextBox.Text))
+                {
+                    command.Parameters.AddWithValue("@username", usernameTextBox.Text);
+                    command.Parameters.AddWithValue("@password", passwordTextBox.Text);
+                    reader = command.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        string role = reader["Role"].ToString();
+
+                        if (role == "Administrator")
+                        {
+                            AdminMainMenu dashboard = new AdminMainMenu(reader["Name"].ToString(), reader["Role"].ToString());
+                            dashboard.Show();
+                            this.Hide();
+                            dashboard.FormClosed += (s, args) => this.Close(); // closes the login form that is hidden when the main program is closed
+                        } else
+                        {
+                            EmployeeMainMenu dashboard = new EmployeeMainMenu(reader["Name"].ToString(), reader["Role"].ToString());
+                            dashboard.Show();
+                            this.Hide();
+                            dashboard.FormClosed += (s, args) => this.Close(); // closes the login form that is hidden when the main program is closed
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Login Error: Username and Password don't match.", "Login Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                } else
+                {
+                    MessageBox.Show("Population Error: Fields cannot be empty.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                conn.Close();
+            } catch (SqlException sqlEx)
+            {
+                MessageBox.Show("SQL Error: " + sqlEx.Message); // displays the SQL error
+            } catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message); // displays any normal error encountered
+            }
+        }
+    }
+}
