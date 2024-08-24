06db0249f93b9e5b488d582db185cad2a4cee25a
FrancoisNWU <40723380@mynwu.ac.za>
2024-08-24 13:51:13 +0200

    Update DVD.Designer.cs

--- CMPG223_Group10_DVD_Rental/DVD.Designer.cs
+++ CMPG223_Group10_DVD_Rental/DVD.Designer.cs
@@ -240,7 +240,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(736, 531);
+            this.ClientSize = new System.Drawing.Size(731, 542);
             this.Controls.Add(this.shelfLabel);
             this.Controls.Add(this.cmbNames);
             this.Controls.Add(this.lblSelectName);
