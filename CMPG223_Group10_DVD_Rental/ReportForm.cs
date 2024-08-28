7605de46a51ec299ac87d145cac066a415967d8a
RohannNWU <25130757@mynwu.ac.za>
2024-08-28 02:48:16 +0200

    Added Report Function

--- CMPG223_Group10_DVD_Rental/ReportForm.cs
+++ CMPG223_Group10_DVD_Rental/ReportForm.cs
@@ -11,10 +11,12 @@
  */
 
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace CMPG223_Group10_DVD_Rental
 {
@@ -48,11 +50,47 @@ namespace CMPG223_Group10_DVD_Rental
 
         private void GetLateReport()
         {
+            reportListBox.Items.Clear();
             conn = new SqlConnection(connectionString);
+            List<int> clientID = new List<int>();
+            List<string> clientNames = new List<string>();
+            List<int> dvdID = new List<int>();
+            List<string> returnDates = new List<string>();
 
             try
             {
+                conn.Open();
+                sqlQuery = "SELECT Client_ID, DVD_ID, Return_Date FROM Rental WHERE Return_Date < @today";
+                command = new SqlCommand(sqlQuery, conn);
+                command.Parameters.AddWithValue("@today", DateTime.Today.ToShortDateString());
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    clientID.Add((int)reader.GetValue(0));
+                    dvdID.Add((int)reader.GetValue(1));
+                    returnDates.Add(Convert.ToDateTime(reader.GetValue(2)).ToString("yyyy/MM/dd"));
+                }
+                conn.Close();
 
+                conn.Open();
+                sqlQuery = "SELECT Client_Name_Surname FROM CLIENT WHERE Client_ID = @client";
+                command = new SqlCommand(sqlQuery, conn);
+                foreach (int id in clientID)
+                {
+                    command.Parameters.AddWithValue("@client", id);
+                    reader = command.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        clientNames.Add(reader.GetValue(0).ToString());
+                    }
+                }
+                conn.Close();
+                reportListBox.Items.Add($"{"Client ID",-12}{"Client Name",-15}\t{"DVD ID",-5}\t{"Return Date"}");
+                for (int i = 0; i < clientID.Count; i++)
+                {
+                    string entry = $"{clientID[i],-14}\t{clientNames[i],-15}\t{dvdID[i],-5}\t{returnDates[i]:yyyy/MM/dd}";
+                    reportListBox.Items.Add(entry);
+                }
             } catch (SqlException sqlEx)
             {
                 MessageBox.Show("Sql Error. " + sqlEx.Message);
