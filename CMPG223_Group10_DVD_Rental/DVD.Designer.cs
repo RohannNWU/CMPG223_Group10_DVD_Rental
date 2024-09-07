f3a6f45bc585bfd1b1f71bce255444e5953b108a
ChristolPrinsloo <114835933+ChristolPrinsloo@users.noreply.github.com>
2024-09-07 15:44:13 +0200

    Enlarge combobox so full command is visible

--- CMPG223_Group10_DVD_Rental/DVD.Designer.cs
+++ CMPG223_Group10_DVD_Rental/DVD.Designer.cs
@@ -57,11 +57,10 @@
             // 
             this.dvdGridView.BackgroundColor = System.Drawing.Color.Silver;
             this.dvdGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dvdGridView.Location = new System.Drawing.Point(25, 91);
-            this.dvdGridView.Margin = new System.Windows.Forms.Padding(4);
+            this.dvdGridView.Location = new System.Drawing.Point(19, 74);
             this.dvdGridView.Name = "dvdGridView";
             this.dvdGridView.RowHeadersWidth = 51;
-            this.dvdGridView.Size = new System.Drawing.Size(911, 185);
+            this.dvdGridView.Size = new System.Drawing.Size(683, 150);
             this.dvdGridView.TabIndex = 0;
             // 
             // lblHeader
@@ -71,10 +70,9 @@
             this.lblHeader.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lblHeader.Font = new System.Drawing.Font("Century Gothic", 20F);
             this.lblHeader.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(198)))), ((int)(((byte)(225)))));
-            this.lblHeader.Location = new System.Drawing.Point(331, 25);
-            this.lblHeader.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblHeader.Location = new System.Drawing.Point(248, 20);
             this.lblHeader.Name = "lblHeader";
-            this.lblHeader.Size = new System.Drawing.Size(327, 40);
+            this.lblHeader.Size = new System.Drawing.Size(264, 33);
             this.lblHeader.TabIndex = 4;
             this.lblHeader.Text = "DVD Management";
             this.lblHeader.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -88,10 +86,9 @@
             "Delete DVD",
             "Update DVD",
             "Search Genre and Location"});
-            this.cmbCommand.Location = new System.Drawing.Point(550, 287);
-            this.cmbCommand.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbCommand.Location = new System.Drawing.Point(412, 233);
             this.cmbCommand.Name = "cmbCommand";
-            this.cmbCommand.Size = new System.Drawing.Size(232, 29);
+            this.cmbCommand.Size = new System.Drawing.Size(223, 28);
             this.cmbCommand.TabIndex = 5;
             this.cmbCommand.SelectedIndexChanged += new System.EventHandler(this.cmbCommand_SelectedIndexChanged);
             // 
@@ -100,10 +97,9 @@
             this.lblCommand.AutoSize = true;
             this.lblCommand.Font = new System.Drawing.Font("Century Gothic", 11F);
             this.lblCommand.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(198)))), ((int)(((byte)(225)))));
-            this.lblCommand.Location = new System.Drawing.Point(317, 290);
-            this.lblCommand.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCommand.Location = new System.Drawing.Point(238, 236);
             this.lblCommand.Name = "lblCommand";
-            this.lblCommand.Size = new System.Drawing.Size(171, 22);
+            this.lblCommand.Size = new System.Drawing.Size(137, 20);
             this.lblCommand.TabIndex = 6;
             this.lblCommand.Text = "Select command:";
             // 
@@ -121,11 +117,9 @@
             this.gbInput.Controls.Add(this.lblName);
             this.gbInput.Font = new System.Drawing.Font("Century Gothic", 11F);
             this.gbInput.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(198)))), ((int)(((byte)(225)))));
-            this.gbInput.Location = new System.Drawing.Point(25, 375);
-            this.gbInput.Margin = new System.Windows.Forms.Padding(4);
+            this.gbInput.Location = new System.Drawing.Point(19, 305);
             this.gbInput.Name = "gbInput";
-            this.gbInput.Padding = new System.Windows.Forms.Padding(4);
-            this.gbInput.Size = new System.Drawing.Size(528, 276);
+            this.gbInput.Size = new System.Drawing.Size(396, 224);
             this.gbInput.TabIndex = 7;
             this.gbInput.TabStop = false;
             this.gbInput.Text = "Input";
@@ -134,9 +128,10 @@
             // cmbDelete
             // 
             this.cmbDelete.FormattingEnabled = true;
-            this.cmbDelete.Location = new System.Drawing.Point(279, 45);
+            this.cmbDelete.Location = new System.Drawing.Point(209, 37);
+            this.cmbDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cmbDelete.Name = "cmbDelete";
-            this.cmbDelete.Size = new System.Drawing.Size(78, 29);
+            this.cmbDelete.Size = new System.Drawing.Size(60, 28);
             this.cmbDelete.TabIndex = 10;
             this.cmbDelete.Visible = false;
             // 
@@ -153,10 +148,9 @@
             "Horror",
             "Romance",
             "Science Fiction"});
-            this.cmbDrop.Location = new System.Drawing.Point(279, 124);
-            this.cmbDrop.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbDrop.Location = new System.Drawing.Point(209, 101);
             this.cmbDrop.Name = "cmbDrop";
-            this.cmbDrop.Size = new System.Drawing.Size(201, 29);
+            this.cmbDrop.Size = new System.Drawing.Size(152, 28);
             this.cmbDrop.TabIndex = 9;
             this.cmbDrop.SelectedIndexChanged += new System.EventHandler(this.cmbDrop_SelectedIndexChanged);
             // 
@@ -167,10 +161,9 @@
             this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSubmit.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Bold);
             this.btnSubmit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(4)))), ((int)(((byte)(8)))));
-            this.btnSubmit.Location = new System.Drawing.Point(209, 219);
-            this.btnSubmit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSubmit.Location = new System.Drawing.Point(157, 178);
             this.btnSubmit.Name = "btnSubmit";
-            this.btnSubmit.Size = new System.Drawing.Size(107, 49);
+            this.btnSubmit.Size = new System.Drawing.Size(80, 40);
             this.btnSubmit.TabIndex = 8;
             this.btnSubmit.Text = "Submit";
             this.btnSubmit.UseVisualStyleBackColor = false;
@@ -179,70 +172,63 @@
             // txtCopies
             // 
             this.txtCopies.Font = new System.Drawing.Font("Century Gothic", 11F);
-            this.txtCopies.Location = new System.Drawing.Point(279, 165);
-            this.txtCopies.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCopies.Location = new System.Drawing.Point(209, 134);
             this.txtCopies.Name = "txtCopies";
-            this.txtCopies.Size = new System.Drawing.Size(201, 30);
+            this.txtCopies.Size = new System.Drawing.Size(152, 25);
             this.txtCopies.TabIndex = 7;
             this.txtCopies.TextChanged += new System.EventHandler(this.txtCopies_TextChanged);
             // 
             // txtYear
             // 
             this.txtYear.Font = new System.Drawing.Font("Century Gothic", 11F);
-            this.txtYear.Location = new System.Drawing.Point(279, 84);
-            this.txtYear.Margin = new System.Windows.Forms.Padding(4);
+            this.txtYear.Location = new System.Drawing.Point(209, 68);
             this.txtYear.Name = "txtYear";
-            this.txtYear.Size = new System.Drawing.Size(201, 30);
+            this.txtYear.Size = new System.Drawing.Size(152, 25);
             this.txtYear.TabIndex = 5;
             this.txtYear.TextChanged += new System.EventHandler(this.txtYear_TextChanged);
             // 
             // txtName
             // 
             this.txtName.Font = new System.Drawing.Font("Century Gothic", 11F);
-            this.txtName.Location = new System.Drawing.Point(279, 45);
-            this.txtName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtName.Location = new System.Drawing.Point(209, 37);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(201, 30);
+            this.txtName.Size = new System.Drawing.Size(152, 25);
             this.txtName.TabIndex = 4;
             this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
             // 
             // lblCopies
             // 
             this.lblCopies.AutoSize = true;
-            this.lblCopies.Location = new System.Drawing.Point(35, 168);
-            this.lblCopies.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCopies.Location = new System.Drawing.Point(26, 136);
             this.lblCopies.Name = "lblCopies";
-            this.lblCopies.Size = new System.Drawing.Size(77, 22);
+            this.lblCopies.Size = new System.Drawing.Size(64, 20);
             this.lblCopies.TabIndex = 3;
             this.lblCopies.Text = "Copies:";
             // 
             // lblYear
             // 
             this.lblYear.AutoSize = true;
-            this.lblYear.Location = new System.Drawing.Point(35, 87);
-            this.lblYear.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblYear.Location = new System.Drawing.Point(26, 71);
             this.lblYear.Name = "lblYear";
-            this.lblYear.Size = new System.Drawing.Size(57, 22);
+            this.lblYear.Size = new System.Drawing.Size(45, 20);
             this.lblYear.TabIndex = 2;
             this.lblYear.Text = "Year:";
             // 
             // lblGenre
             // 
             this.lblGenre.AutoSize = true;
-            this.lblGenre.Location = new System.Drawing.Point(35, 127);
-            this.lblGenre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblGenre.Location = new System.Drawing.Point(26, 103);
             this.lblGenre.Name = "lblGenre";
-            this.lblGenre.Size = new System.Drawing.Size(79, 22);
+            this.lblGenre.Size = new System.Drawing.Size(64, 20);
             this.lblGenre.TabIndex = 1;
             this.lblGenre.Text = "Genre: ";
             // 
             // lblName
             // 
             this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(35, 48);
-            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblName.Location = new System.Drawing.Point(26, 39);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(76, 22);
+            this.lblName.Size = new System.Drawing.Size(61, 20);
             this.lblName.TabIndex = 0;
             this.lblName.Text = "Name: ";
             // 
@@ -251,10 +237,9 @@
             this.lblSelectName.AutoSize = true;
             this.lblSelectName.Font = new System.Drawing.Font("Century Gothic", 11F);
             this.lblSelectName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(198)))), ((int)(((byte)(225)))));
-            this.lblSelectName.Location = new System.Drawing.Point(317, 331);
-            this.lblSelectName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSelectName.Location = new System.Drawing.Point(238, 269);
             this.lblSelectName.Name = "lblSelectName";
-            this.lblSelectName.Size = new System.Drawing.Size(177, 22);
+            this.lblSelectName.Size = new System.Drawing.Size(141, 20);
             this.lblSelectName.TabIndex = 8;
             this.lblSelectName.Text = "Select DVD name:";
             this.lblSelectName.Visible = false;
@@ -263,10 +248,9 @@
             // 
             this.cmbNames.Font = new System.Drawing.Font("Century Gothic", 11F);
             this.cmbNames.FormattingEnabled = true;
-            this.cmbNames.Location = new System.Drawing.Point(550, 324);
-            this.cmbNames.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbNames.Location = new System.Drawing.Point(412, 263);
             this.cmbNames.Name = "cmbNames";
-            this.cmbNames.Size = new System.Drawing.Size(232, 29);
+            this.cmbNames.Size = new System.Drawing.Size(223, 28);
             this.cmbNames.TabIndex = 9;
             this.cmbNames.Visible = false;
             this.cmbNames.SelectedIndexChanged += new System.EventHandler(this.cmbNames_SelectedIndexChanged);
@@ -276,9 +260,10 @@
             this.shelfLabel.AutoSize = true;
             this.shelfLabel.Font = new System.Drawing.Font("Century Gothic", 11F);
             this.shelfLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(198)))), ((int)(((byte)(225)))));
-            this.shelfLabel.Location = new System.Drawing.Point(618, 502);
+            this.shelfLabel.Location = new System.Drawing.Point(464, 408);
+            this.shelfLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.shelfLabel.Name = "shelfLabel";
-            this.shelfLabel.Size = new System.Drawing.Size(142, 22);
+            this.shelfLabel.Size = new System.Drawing.Size(115, 20);
             this.shelfLabel.TabIndex = 10;
             this.shelfLabel.Text = "Shelf Location:";
             this.shelfLabel.Visible = false;
@@ -289,10 +274,10 @@
             // 
             // DVD
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(60)))), ((int)(((byte)(93)))));
-            this.ClientSize = new System.Drawing.Size(956, 664);
+            this.ClientSize = new System.Drawing.Size(717, 540);
             this.Controls.Add(this.shelfLabel);
             this.Controls.Add(this.cmbNames);
             this.Controls.Add(this.lblSelectName);
@@ -302,7 +287,6 @@
             this.Controls.Add(this.lblHeader);
             this.Controls.Add(this.dvdGridView);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "DVD";
